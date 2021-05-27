df86588ad4ddf9d4a27f867f012aa6223f7c644f
anitaRead <areadman91@gmail.com>
2021-05-27 10:33:32 +0100

    Add copy file for exercises

---
+++ directory_copy.rb
@@ -0,0 +1,35 @@
+def input_students
+  puts "Please enter the names of students"
+  puts "To finish, just hit return twice" 
+  name = gets.chomp 
+
+  students = []
+
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end  
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
