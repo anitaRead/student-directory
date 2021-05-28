45a65fc3e8f82499e31fa131a76cfe9f48c6b9b8
anitaRead <areadman91@gmail.com>
2021-05-28 11:49:26 +0100

    Add method to save students

--- directory_copy.rb
+++ directory_copy.rb
@@ -66,6 +66,7 @@ def print_menu
   # Print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" 
 end
 
@@ -81,6 +82,8 @@ def process(selection)
     @students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -93,6 +96,16 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  file = File.open("students.csw", "w")
+  @students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
